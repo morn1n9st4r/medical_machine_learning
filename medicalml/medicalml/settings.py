3ccf2acd5b30d4952d86a6cc641b0833b83e238c
morn1n9st4r <tutgdeto2001@gmail.com>
2023-09-30 22:53:08 +0300

    logging in

--- medicalml/medicalml/settings.py
+++ medicalml/medicalml/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'crispy_bootstrap5'
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS ="bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -128,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'
